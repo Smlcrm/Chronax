73d3228f4535d48793071b8c1119d389456f6b31
Xan Carey <xan.carey@gmail.com>
2025-10-14 12:55:04 -0400

    added instance attribute method

--- conformal_intervals.py
+++ conformal_intervals.py
@@ -8,11 +8,12 @@ Instance Attributes
 """
 
 class ConformalIntervals:
-    # __slots__ = ('n_windows, 'h')
+    # __slots__ = ('n_windows, 'h', 'method')
     def __init__(
         self,
         n_windows: int = 2,
         h: int = 1,
+        method: str = "conformal_distribution",
     ):
         if n_windows < 2:
             raise ValueError(
@@ -20,3 +21,4 @@ class ConformalIntervals:
             )
         self.n_windows = n_windows
         self.h = h
+        self.method = method
