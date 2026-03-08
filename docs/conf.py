217b9921319b00c265cd0384af2e61690f419e4b
Xan Carey <xan@Mac.local>
2026-03-08 15:32:22 -0400

    improved readthedocs aesthetics

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,8 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
 ]
 
 # Napoleon settings for NumPy-style docstrings
@@ -20,5 +22,22 @@ napoleon_numpy_docstring = True
 # Autodoc settings
 autodoc_member_order = "bysource"
 
+# Intersphinx: cross-reference JAX, NumPy, and Python docs
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "jax": ("https://jax.readthedocs.io/en/latest", None),
+    "optax": ("https://optax.readthedocs.io/en/latest", None),
+}
+
+# Copybutton: strip common prompts from copied code
+copybutton_prompt_text = r">>> |\.\.\. |\$ "
+copybutton_prompt_is_regexp = True
+
 # HTML output
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "github_url": "https://github.com/Smlcrm/ml-library-chronax",
+    "show_prev_next": False,
+    "navbar_align": "left",
+}
